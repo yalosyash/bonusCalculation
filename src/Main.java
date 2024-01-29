0c0357facfd90f3d908080f3fce395eb4bdb9657
Клепиков Владислав Андреевич <klepikov@domvod.local>
2024-01-29 15:38:30 +0300

    add feature

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        int price = 13676;
-        int bonusPrice = 20;
-
-        int bonusCount = price / bonusPrice;
-
-        System.out.print(bonusCount);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
+
 }
